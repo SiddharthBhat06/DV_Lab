910fbab9727363d6fdc283db14dc19ce7baf83ba
Siddharth Bhat <siddharthbhat99@gmail.com>
2025-01-31 15:09:25 +0530

    Create Q1.py

---
+++ Lab-1/Q1.py
@@ -0,0 +1,21 @@
+def calculator():
+    print("Python Calculator")
+    try:
+        a= float(input("Enter Number 1 :"))
+        b= float(input("Enter Number 2 :"))
+        c= int(input("Enter choice of operation : 1-Addition, 2-Subtraction, 3-Multiplication, 4-Division"))
+        if c in range(1,5):
+            if c==1:
+                print("Sum is :",a+b)
+            elif c==2:
+                print("Difference is :",a-b)
+            elif c==3:
+                print("Product is :",a*b)
+            elif c==4:
+                print("Quotient is :",a/b)
+        else:
+            print("Invalid input")
+
+    except:
+        print("Invalid input")
+calculator()
